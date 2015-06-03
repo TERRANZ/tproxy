1baf72359f8dcd420bd1c850d4a8cef7fda74cef
TERRANZ <terranz@terranz.ath.cx>
2015-06-03 12:21:34 +0400

    fix

--- android_studio/app/src/main/java/ru/terra/tproxy/Config.java
+++ android_studio/app/src/main/java/ru/terra/tproxy/Config.java
@@ -4,7 +4,7 @@ package ru.terra.tproxy;
  * Created by terranz on 01.06.15.
  */
 public class Config {
-    public static final String IP = "";
-    public static final String LOGIN = "";
-    public static final String PASS = "";
+    public static final String IP = "91.229.78.210";
+    public static final String LOGIN = "androtest";
+    public static final String PASS = "andropa$$";
 }
--- android_studio/app/src/main/java/ru/terra/tproxy/service/ProxyService.java
+++ android_studio/app/src/main/java/ru/terra/tproxy/service/ProxyService.java
@@ -11,7 +11,6 @@ import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
@@ -22,7 +21,6 @@ import org.littleshoot.proxy.HttpProxyServer;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Random;
 
 import ru.terra.tproxy.Config;
@@ -39,7 +37,7 @@ public class ProxyService extends IntentService {
     private volatile boolean run;
     private Session session;
     private LocalBroadcastManager lbm;
-    private Channel chan;
+    //    private Channel chan;
     private HttpProxyServer proxy;
     private boolean forward = true;
     private SharedPreferences preferences;
@@ -69,6 +67,11 @@ public class ProxyService extends IntentService {
             @Override
             public void run() {
                 while (run) {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     if (session != null)
                         if (!session.isConnected()) {
                             try {
@@ -136,8 +139,8 @@ public class ProxyService extends IntentService {
     private void fullStop() {
         if (proxy != null)
             proxy.stop();
-        if (chan != null)
-            chan.disconnect();
+//        if (chan != null)
+//            chan.disconnect();
         if (session != null)
             session.disconnect();
         lbm.sendBroadcast(new Intent(MainActivity.STATUS_RECEIVER).putExtra("text", "Stopped"));
@@ -179,63 +182,44 @@ public class ProxyService extends IntentService {
         String host = /*preferences.getString(getString(R.string.desktop_addr), "192.168.1.4");*/ Config.IP;
         String user = /*preferences.getString(getString(R.string.login_key), "");*/ Config.LOGIN;
         String password =/*preferences.getString(getString(R.string.pass_key), "");*/ Config.PASS;
-        int port = Integer.parseInt(preferences.getString(getString(R.string.desktop_port_key), "23"));
+        int port = Integer.parseInt(preferences.getString(getString(R.string.desktop_port_key), "22"));
 
         int tunnelLocalPort = Integer.parseInt(preferences.getString(getString(R.string.port_key), "55555"));
         String tunnelRemoteHost = "127.0.0.1";
-        int tunnelRemotePort = new Random().nextInt();
+        int tunnelRemotePort = randInt(10000, 50000);
+        Log.d(TAG, "Remote port " + tunnelRemotePort);
 
         JSch jsch = new JSch();
         session = jsch.getSession(user, host, port);
         session.setPassword(password);
         localUserInfo lui = new localUserInfo(password);
         session.setUserInfo(lui);
-
         session.connect();
 
-        if (session != null) {
-            if (f)
-                session.setPortForwardingR("0.0.0.0", tunnelLocalPort, tunnelRemoteHost, tunnelRemotePort);
-            chan = session.openChannel("shell");
-            chan.connect();
-            final InputStream in = chan.getInputStream();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        byte[] tmp = new byte[1024];
-                        while (true) {
-                            while (in.available() > 0) {
-                                int i = in.read(tmp, 0, 1024);
-                                if (i < 0) break;
-                                String res = new String(tmp, 0, i);
-                                Log.i(TAG, "Received " + res);
-                                if ("forward".equalsIgnoreCase(res)) {
-                                    forward = true;
-                                    lbm.sendBroadcast(new Intent(ProxyService.PROXY_RECEIVER).putExtra("forward", true));
-                                } else if ("restart".equalsIgnoreCase(res)) {
-                                    forward = false;
-                                    lbm.sendBroadcast(new Intent(ProxyService.PROXY_RECEIVER).putExtra("restart", true));
-                                }
-                                break;
-                            }
-                            if (chan.isClosed()) {
-                                if (in.available() > 0) continue;
-                                Log.i(TAG, "exit-status: " + chan.getExitStatus());
-                                System.out.println("exit-status: " + chan.getExitStatus());
-//                                    fullStop();
-                                break;
-                            }
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception ee) {
-                            }
-                        }
-                    } catch (Exception e) {
-                        Log.e("ProxyService", e.getMessage(), e);
-                    }
-                }
-            }).start();
-        }
+        if (session != null)
+            session.setPortForwardingR("0.0.0.0", tunnelRemotePort, tunnelRemoteHost, tunnelLocalPort);
+    }
+
+    /**
+     * Returns a pseudo-random number between min and max, inclusive.
+     * The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
     }
 }
